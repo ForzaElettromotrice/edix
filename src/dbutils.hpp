3f2946123fd36ed5a5fd6fccae627abc6f36db82
simonerufo <simonerufo3@gmail.com>
2024-01-13 12:13:10 +0100

    aggiornati i file db utils, rimuovendo le funzioni che si riferivano all'utilizzo di redis

--- src/dbutils.hpp
+++ src/dbutils.hpp
@@ -9,21 +9,22 @@
 #include <hiredis/hiredis.h>
 #include <cstdlib>
 #include <cstring>
+#include "rdutils.hpp"
 #include "utils.hpp"
 
 
 int initDb();
 int checkDb();
 int loadProjectOnRedis(char *projectName);
-int upload_to_redis(int id, char *tup, char *mod_ex, char *comp, unsigned int tts, char *tpp, bool vcs, int project);
+//int upload_to_redis(int id, char *tup, char *mod_ex, char *comp, unsigned int tts, char *tpp, bool vcs, int project);
 int addProject(char *name, char *path, char *comp, char *TPP, char *TUP, char *modEx, uint TTS, bool VCS);
 
 
 //UTILS
-int get_project_id(char *projectName, char **ID);
-int get_check(char *name, redisReply *reply, redisContext *context);
-int set_check(char *name, redisReply *reply, redisContext *context);
-char **get_settings(PGconn *conn, char *projectId);
+int getProjectId(char *projectName, char **ID);
+//int get_check(char *name, redisReply *reply, redisContext *context);
+//int set_check(char *name, redisReply *reply, redisContext *context);
+char **getSettings(PGconn *conn, char *projectId);
 
 bool checkRoleExists(PGconn *conn, const char *roleName);
 bool checkDatabaseExists(PGconn *conn, const char *dbName);
