e1603cb92a367e0602840f38ec56b4bdc96c0ca9
simonerufo <simonerufo3@gmail.com>
2024-01-14 15:50:11 +0100

    riscritto le funzioni brutte settingToRedis e settingsFromRedis

--- src/rdutils.cpp
+++ src/rdutils.cpp
@@ -85,38 +85,16 @@ int settingsToRedis(int id, char *tup, char *mod_ex, char *comp, u_int tts, char
     initRedis(&context);
 
     //send settings to redis
-    auto *reply = (redisReply *) redisCommand(context, "SET ID %d", id);
-    setChecking((char *) "ID", reply, context);
-
-
-    reply = (redisReply *) redisCommand(context, "SET Project %d", project);
-    setChecking((char *) "Project", reply, context);
-
-
-    reply = (redisReply *) redisCommand(context, "SET Mod_ex %s", mod_ex);
-    setChecking((char *) "Mod_ex", reply, context);
-
-    reply = (redisReply *) redisCommand(context, "SET TTS %u", tts);
-    setChecking((char *) "TTS", reply, context);
-
-    reply = (redisReply *) redisCommand(context, "SET VCS %d", vcs);
-    setChecking((char *) "VCS", reply, context);
-
-
-    reply = (redisReply *) redisCommand(context, "SET COMP %s", comp);
-    setChecking((char *) "COMP", reply, context);
-
-    reply = (redisReply *) redisCommand(context, "SET TPP %s", tpp);
-    setChecking((char *) "TPP", reply, context);
-
-
-    reply = (redisReply *) redisCommand(context, "SET TUP %s", tup);
-    setChecking((char *) "TUP", reply, context);
-
+    setKeyValueInt("ID",id);
+    setKeyValueStr("Project",project);
+    setKeyValueStr("Mod_ex",mod_ex);
+    setKeyValueInt("TTS",tts);
+    setKeyValueInt("VCS",vcs);
+    setKeyValueStr("COMP",comp);
+    setKeyValueStr("TPP",tpp);
+    setKeyValueStr("TUP",tup);
 
     //End redis connection
-
-    freeReplyObject(reply);
     redisFree(context);
 
     return 0;
@@ -133,37 +111,24 @@ int settingsFromRedis(int *id, char **tup, char **mod_ex, char **comp, u_int *tt
     //TODO: poi sta funzione non me piace, ce deve esse un modo piu intelligente di farla
 
     // Recupera i dati dalle chiavi
-    auto *reply = (redisReply *) redisCommand(context, "GET ID");
-    getChecking((char *) "ID", reply, context);
-    *id = (int) reply->integer;
+ 
+    *id = getIntFromKey("ID");
 
-    reply = (redisReply *) redisCommand(context, "GET Project");
-    getChecking((char *) "Project", reply, context);
-    *project = reply->str;
+    *project = getStrFromKey("Project");
 
-    reply = (redisReply *) redisCommand(context, "GET Mod_ex");
-    getChecking((char *) "Mod_ex", reply, context);
-    *mod_ex = reply->str;
+    *mod_ex = getStrFromKey("Mod_ex");
 
-    reply = (redisReply *) redisCommand(context, "GET TTS");
-    getChecking((char *) "TTS", reply, context);
-    *tts = reply->integer;
+    *tts = getIntFromKey("TTS");
 
-    reply = (redisReply *) redisCommand(context, "GET VCS");
+    auto *reply = (redisReply *) redisCommand(context, "GET VCS");
     getChecking((char *) "VCS", reply, context);
     *vcs = reply->integer == 1;
 
-    reply = (redisReply *) redisCommand(context, "GET COMP");
-    getChecking((char *) "COMP", reply, context);
-    *comp = reply->str;
+    *comp = getStrFromKey("COMP");
 
-    reply = (redisReply *) redisCommand(context, "GET TPP");
-    getChecking((char *) "TPP", reply, context);
-    *tpp = reply->str;
+    *tpp = getStrFromKey("TPP");
 
-    reply = (redisReply *) redisCommand(context, "GET TUP");
-    getChecking((char *) "TUP", reply, context);
-    *tup = reply->str;
+    *tup = getStrFromKey("TUP");
 
     //TODO: printa tutti i valori e compara con i SET
     //End redis connection
