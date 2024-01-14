9ab4516547ab5da438930aa3b9b55aa41794da9d
simonerufo <simonerufo3@gmail.com>
2024-01-14 13:37:21 +0100

    inserite le funzioni setter e getter in rdutils, e aggiunta la funzione checkRedisService

--- src/rdutils.cpp
+++ src/rdutils.cpp
@@ -31,27 +31,49 @@ int initRedis(redisContext **context)
     }
     return 0;
 }
+//check if connection is alive
+int checkRedisService(){
 
+    redisContext *context;
+    context = redisConnect("localhost", 6379);
+
+    if (context == nullptr || (context)->err)
+    {
+        if (context)
+        {
+            fprintf(stderr, "Error while connecting to redis: %s\n", (context)->errstr);
+            redisFree(context);
+        } else
+            fprintf(stderr, "Unable to initialize connection to redis\n");
+
+
+        return 1;
+    }
+    return 0;
+}
+
+//check if set redis command is valid
 int setChecking(char *name, redisReply *reply, redisContext *context)
 {
     if (reply == nullptr)
     {
         freeReplyObject(reply);
         redisFree(context);
-        handle_error("Error while executing redis command SET %s\n", name);
+        handle_error("Error while executing redis SET command %s\n", name);
     }
 
     return 0;
 }
 
-
+//check if get redis command is valid
 int getChecking(char *name, redisReply *reply, redisContext *context)
 {
     if (reply == nullptr)
     {
+        handle_error("Error while executing GET command %s\n", name);
         freeReplyObject(reply);
         redisFree(context);
-        handle_error("Errore nell'esecuzione del comando GET %s\n", name);
+        return 1;
     }
     return 0;
 }
@@ -153,16 +175,101 @@ int settingsFromRedis(int *id, char **tup, char **mod_ex, char **comp, u_int *tt
 
 }
 
-//TODO GET FROM REDIS USING KEY
-redisReply *getFromKey(char *key)
-{
+// set to redis using key,value
+int setKeyValueStr(char *key, char *value){
+    //init connection to redis
+    redisContext *context;
+    initRedis(&context);
+
+    //set key,value to redis
+    auto *reply = (redisReply*)redisCommand(context,"SET %s %s", key, value);
+    setChecking(key,reply,context);
+
+    freeReplyObject(reply);
+    //set persistent keys
+    auto *persist = (redisReply*)redisCommand(context,"PERSIST %s",key);
+
+    if (persist == NULL || persist->type == REDIS_REPLY_ERROR) {
+        printf("Error while setting PERSISTENT to key\n");
+        redisFree(context);
+        return -1;
+    }
+    freeReplyObject(persist);
+    //free space
+    redisFree(context);
+
+    return 0;
+}
+
+int setKeyValueInt(char *key, int value){
+    //init connection to redis
+    redisContext *context;
+    initRedis(&context);
+
+    //set key,value to redis
+    auto *reply = (redisReply*)redisCommand(context,"SET %s %d", key, value);
+    setChecking(key,reply,context);
+
+    freeReplyObject(reply);
+    //set persistent keys
+    auto *persist = (redisReply*)redisCommand(context,"PERSIST %s",key);
+
+    if (persist == NULL || persist->type == REDIS_REPLY_ERROR) {
+        printf("Error while setting PERSISTENT to key\n");
+        redisFree(context);
+        return -1;
+    }
+    freeReplyObject(persist);
+    //free space
+    redisFree(context);
+    
+    return 0;
+}
+
+
+//GET values FROM REDIS USING KEY
+char* getStrFromKey(char *key){
     //initialize connection
     redisContext *context;
     initRedis(&context);
 
-    //todo get from key
+    //getting string from redis
+    auto reply = (redisReply *) redisCommand(context, "GET %s",key);
+    getChecking(key,reply,context);
 
+    char *value;
+    if(reply->type == REDIS_REPLY_STRING){
+        value = reply->str;
+    }else{
+        fprintf(stderr,"the object you received is not a str!");
+        return nullptr;
+    }
+
+    freeReplyObject(reply);
     //end connection;
     redisFree(context);
-    return nullptr;
+    return value;
+}
+
+int  getIntFromKey(char *key){
+    //initialize connection
+    redisContext *context;
+    initRedis(&context);
+
+    //getting string from redis
+    auto reply = (redisReply *) redisCommand(context, "GET %s",key);
+    getChecking(key,reply,context);
+
+    int value;
+    if(reply->type == REDIS_REPLY_STRING){
+        value = strtol(reply->str,nullptr,10);
+    }else{
+        fprintf(stderr,"the object you received is not a str!");
+        return -9999;
+    }
+
+    freeReplyObject(reply);
+    //end connection;
+    redisFree(context);
+    return value;
 }
--- src/rdutils.hpp
+++ src/rdutils.hpp
@@ -7,3 +7,7 @@ int setChecking(char *name, redisReply *reply, redisContext *context);
 int getChecking(char *name, redisReply *reply, redisContext *context);
 int settingsToRedis(int id, char *tup, char *mod_ex, char *comp, u_int tts, char *tpp, bool vcs, int project);
 int settingsFromRedis(int *id, char **tup, char **mod_ex, char **comp, u_int *tts, char **tpp, bool *vcs, int *project);
+int checkRedisService();
+redisReply* getFromKey(char *key);
+int setKeyValueInt(char *key, int value);
+int setKeyValueStr(char *key, char *value);
