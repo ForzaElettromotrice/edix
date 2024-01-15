c81718e6b8b8384f823c06c1b4ea8e29d23baa76
simonerufo <simonerufo3@gmail.com>
2024-01-15 11:01:54 +0100

    bug fix

--- src/rdutils.hpp
+++ src/rdutils.hpp
@@ -5,7 +5,7 @@
 int initRedis(redisContext **context);
 int setChecking(char *name, redisReply *reply, redisContext *context);
 int getChecking(char *name, redisReply *reply, redisContext *context);
-int settingsToRedis(int id, char *tup, char *mod_ex, char *comp, u_int tts, char *tpp, bool vcs, char  *pName)
+int settingsToRedis(int id, char *tup, char *mod_ex, char *comp, u_int tts, char *tpp, bool vcs, char  *pName);
 int settingsFromRedis(int *id, char **tup, char **mod_ex, char **comp, u_int *tts, char **tpp, bool *vcs, char **pName);
 int checkRedisService();
 int setKeyValueInt(char *key, int value);
